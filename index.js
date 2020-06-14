cba8fbc669f0c9129c89f8e336a817a6fb4b0f80
Eduardo Singelo <eduardo.s.junior@hotmail.com>
2020-06-14 19:41:28 -0300

    Quinto commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const server = express()
+
+server.use(express.static("public"))
+
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+} )
+
+server.get("/create-post", (req, res) => {
+    res.sendFile(__dirname + "/views/create-post.html")
+} )
+
+server.get("/search-results", (req, res) => {
+    res.sendFile(__dirname + "/views/search-results.html")
+} )
+
+server.listen(process.env.PORT || 5000)
